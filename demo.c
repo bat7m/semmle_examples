d44df948d40b80aaef0059de113787d05a489648
bat7m <57671421+bat7m@users.noreply.github.com>
2019-11-25 10:42:07 +0200

    Add files via upload

--- demo.c
+++ demo.c
@@ -13,20 +13,13 @@ int fsize(FILE *fp){
 
 int main(unsigned int argc, char* argv[])
 {
-    unsigned int x = strlen(argv[1]);
-
-// TODO: fix this overflow please
-    if(argc + x < argc )
-        printf("smaller");
-    else
-        printf("bigger");
-
-
 
 	char source[MAXBUFLEN + 1];
 	FILE *fp = fopen("foo.txt", "r");
 	if (fp != NULL) {
-		size_t newLen = fread(source, sizeof(char), fsize(fp), fp);
+		int fileSize = fsize(fp);
+		//overflow
+		size_t newLen = fread(source, sizeof(char), fileSize, fp);
 		if ( ferror( fp ) != 0 ) {
 			fputs("Error reading file", stderr);
 		} else {
@@ -35,9 +28,21 @@ int main(unsigned int argc, char* argv[])
 
 		fclose(fp);
 	}
+	
+	char source2[MAXBUFLEN + 1];
+	fp = fopen("foo.txt", "r");
+	if (fp != NULL) {
+		int fileSize2 = fsize(fp);
+		//no overflow
+		size_t newLen = fread(source2, sizeof(char), fileSize2, fp);
+		if ( ferror( fp ) != 0 ) {
+			fputs("Error reading file", stderr);
+		} else {
+			source[newLen++] = '\0'; /* Just to be safe. */
+		}
 
-
-
+		fclose(fp);
+	}
 
     return 0;
 }
